7600df728ffe7cffea4855c0b41f273f83eff3b4
pigpenguin <ethan@rooke.org>
2013-03-10 15:29:28 -0700

    added the pulley subsytems

--- src/com/team1160/feathers/api/Constants.java
+++ src/com/team1160/feathers/api/Constants.java
@@ -7,6 +7,54 @@ public interface Constants {
 	int DT_LEFT_JAG_CAR = 2;
 	int DT_RIGHT_JAG_CHAN = 3;
 	int DT_RIGHT_JAG_CAR = 1;
+
+	//Pulley System
+	// Top Pulley
+		int P_TOP_JAG_CHAN = 1;
+		int P_TOP_JAG_CAR = 1;
+		int P_TOP_SERVO_CHAN = 5;
+		int P_TOP_SERVO_CAR = 1;
+		double P_TOP_LOCK_LOCKED = .9;
+		double P_TOP_LOCK_OPEN = .1;
+		double P_TOP_HOME = .30;
+		double P_TOP_MAX = .9;
+		double P_TOP_MIN = .2;
+	// Right Pulley
+		int P_RIGHT_JAG_CHAN = 2;
+		int P_RIGHT_JAG_CAR = 1;
+		int P_RIGHT_SERVO_CHAN = 6;
+		int P_RIGHT_SERVO_CAR = 1;
+		int P_RIGHT_LOCK_CHAN = 4;
+		int P_RIGHT_LOCK_CAR = 1;
+		double P_RIGHT_LOCK_LOCKED = .5;
+		double P_RIGHT_LOCK_OPEN = .8;
+		double P_RIGHT_HOME = .63;
+		double P_RIGHT_MAX = 1;
+		double P_RIGHT_MIN = .63;
+	// Left Pulley
+		int P_LEFT_JAG_CHAN = 1;
+		int P_LEFT_JAG_CAR = 2;
+		int P_LEFT_SERVO_CHAN = 4;
+		int P_LEFT_SERVO_CAR = 2;
+		int P_LEFT_LOCK_CHAN = 5;
+		int P_LEFT_LOCK_CAR = 2;
+		double P_LEFT_LOCK_LOCKED = .13;
+		double P_LEFT_LOCK_OPEN = .5;
+		double P_LEFT_HOME = .62;
+		double P_LEFT_MAX =.62;
+		double P_LEFT_MIN = .2;
+// Arm system
+	// Arm
+		int A_MOTOR_CHAN = 8;   
+		int A_MOTOR_CAR =2;                       
+	// Griper
+		int G_SERVO_CHAN = 9;
+		int G_SERVO_CAR = 1;
+		double G_SERVO_GRIP = .9;	
+		double G_SERVO_OPEN = .1;
+
+
+	
 	//Human Input
 	int DRIVESTICK = 1;
 	
