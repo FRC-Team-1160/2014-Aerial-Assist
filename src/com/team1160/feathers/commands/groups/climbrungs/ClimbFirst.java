1a7fe7642fd311ec6d4b04b14d5e440e3f1b09cd
Z-ach <zachkaufman97@gmail.com>
2013-03-16 12:58:17 -0700

    firstrung being worked on + constructor fix

---
+++ src/com/team1160/feathers/commands/groups/climbrungs/ClimbFirst.java
@@ -0,0 +1,11 @@
+package com.team1160.feathers.commands.groups.climbrungs;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class ClimbFirst extends CommandGroup{
+    
+    public ClimbFirst(){
+        //addParallel(LeftPulleyAngle());
+    }
+
+}
