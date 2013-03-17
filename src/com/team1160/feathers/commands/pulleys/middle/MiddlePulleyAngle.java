7b7827095695f6b7a58a98b5017296d6eabbcc70
Z-ach <zachkaufman97@gmail.com>
2013-03-16 21:41:30 -0700

    climbing

--- src/com/team1160/feathers/commands/pulleys/middle/MiddlePulleyAngle.java
+++ src/com/team1160/feathers/commands/pulleys/middle/MiddlePulleyAngle.java
@@ -1,16 +1,23 @@
 package com.team1160.feathers.commands.pulleys.middle;
 
+import com.team1160.feathers.SI;
+import com.team1160.feathers.api.FrameMath;
 import com.team1160.feathers.commands.CommandBase;
 
 public class MiddlePulleyAngle extends CommandBase{
     
+    FrameMath meth;
+    SI si;
     
     public MiddlePulleyAngle(){
+        if (si == null){
+            si = SI.getInstance();
+        }
         requires(middlePulley);
     }
     
     protected void execute(){
-        middlePulley.joyAngle();
+        middlePulley.setAngle(meth.calcServoFromAngle(true, 45, 10, si.getAngle()));
     }
     
     protected boolean isFinished(){
