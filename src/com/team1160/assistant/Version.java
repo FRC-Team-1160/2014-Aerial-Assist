f2e2e75311d9e1c07d003578be7dcccd2a842240
kavigupta <kavi@globalmbox.com>
2014-01-25 22:11:18 -0800

    Optimized some general code for legibility / speed...

--- src/com/team1160/assistant/Version.java
+++ src/com/team1160/assistant/Version.java
@@ -3,33 +3,22 @@ package com.team1160.assistant;
 import java.util.Random;
 
 public class Version {
-
-    public String s1;
-    final double version = 0.1;
-    
-    public static Version instance = null;
-    
-    public static Version getInstance(){
-        if(instance == null){
-            instance = new Version();
-        }
-        return instance;
-    }
-    
-    private Version() {
-
-        Random rand = new Random();
-        int a = rand.nextInt(3);
-
-        if (a == 0) {
-            s1 = "Heh...";
-        } else if (a == 1) {
-            s1 = "Gabriel and Orangutan are red COUCHES.";
-        } else if (a == 2) {
-            s1 = "Zach is a god.";
-        } else if (a == 3) {
-            s1 = "That's a dick move.";
-        }
-        System.out.println("Successful deploy. Currently: v" + version + "Random message: " + s1);
-    }
+	public static String[] RANDOM_MESSAGES = new String[] { "Heh...",
+			"Gabriel and Orangutan are red COUCHES.", "Zach is a god.",
+			"That's a dick move." };
+	final double version = 0.1;
+	public static Version instance = null;
+	public static Version getInstance() {
+		if (instance == null) {
+			instance = new Version();
+		}
+		return instance;
+	}
+	private Version() {
+		System.out.println("Successful deploy. Currently: v"
+				+ version
+				+ "Random message: "
+				+ RANDOM_MESSAGES[new Random()
+						.nextInt(RANDOM_MESSAGES.length)]);
+	}
 }
