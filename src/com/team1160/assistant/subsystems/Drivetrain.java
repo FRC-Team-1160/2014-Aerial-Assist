5022be27c233fb9bc03ce0db0dd20e3422ff942d
Joe Chen <chenjoe569@gmail.com>
2014-02-02 11:35:17 -0800

    possible PID control to subsystems? maybe..?
    hmmm..i thought i posted this…I'm not sure if it works, so feel free to
    edit or delete them

--- src/com/team1160/assistant/subsystems/Drivetrain.java
+++ src/com/team1160/assistant/subsystems/Drivetrain.java
@@ -3,12 +3,16 @@ package com.team1160.assistant.subsystems;
 import com.team1160.assistant.OI;
 import com.team1160.assistant.RobotMap;
 import com.team1160.assistant.commands.Drive.arcadeDrive;
+import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
-public class Drivetrain extends Subsystem implements RobotMap {
+
+public class Drivetrain extends PIDSubsystem implements RobotMap {
 	// LOCAL VARIABLES - WILL BE ASSIGNED VIA CHECKS
 	protected final Jaguar rightJ, leftJ;
 	protected final Joystick stick;
@@ -16,6 +20,11 @@ public class Drivetrain extends Subsystem implements RobotMap {
 	// INSTANCE OF DRIVETRAIN CLASS
 	protected final Encoder encLeft;
 	protected final Encoder encRight;
+        //INSTANCE OF SPEEDCONTROLLER AND ANALOG CHANNELS 
+        protected final SpeedController rightmotorcontrol, leftmotorcontrol; 
+        protected final AnalogChannel  rightmotorchan, leftmotorchan; 
+        protected final double SETPOINT =0; 
+        protected final double ABSOLUTE_TOLERANCE =0; 
 	// METHOD CALLED BY COMMANDBASE TO RETRIEVE INSTANCE OF DT
 	public static Drivetrain getInstance() {
 		// IF DT INSTANCE DOESN'T EXIST, CREATE IT
@@ -28,14 +37,30 @@ public class Drivetrain extends Subsystem implements RobotMap {
 	}
 	// CONSTRUCTOR - IS CALLED ONCE, ONLY BY GETINSTANCE
 	private Drivetrain() {
+            super("Drivetrain", 0, 0, 0); //numerical values PID will be changed to experimental value 
 		// JAGUAR INITIALIZATION
-		rightJ = new Jaguar(LEFT_JAG_MOTOR_SLOT, LEFT_JAG_MOTOR_CHAN);
+		rightJ = new Jaguar(LEFT_JAG_MOTOR_SLOT, RIGHT_JAG_MOTOR_CHAN);
 		leftJ = new Jaguar(RIGHT_JAG_MOTOR_SLOT, LEFT_JAG_MOTOR_CHAN);
 		encLeft = new Encoder(ENC_DT_LEFT_A, ENC_DT_LEFT_B, true);
 		encRight = new Encoder(ENC_DT_RIGHT_A, ENC_DT_RIGHT_B, true);
 		stick = OI.getInstance().getJoystick();
 		encLeft.start();
 		encRight.start();
+                //speed control 
+                rightmotorcontrol = rightJ; 
+                leftmotorcontrol = leftJ; 
+                rightmotorchan = new AnalogChannel(RobotMap.ENC_DT_RIGHT_A); //connected to one of the channels
+                leftmotorchan = new AnalogChannel(RobotMap.ENC_DT_LEFT_A); //same as above
+                getPIDController().setContinuous(false); 
+                LiveWindow.addActuator("Drivetrain", "PIDSubsystem", getPIDController()); 
+                setAbsoluteTolerance(ABSOLUTE_TOLERANCE); 
+                rightmotorcontrol.set(SETPOINT); 
+                leftmotorcontrol.set(SETPOINT); //I think these set up the setpoints; if not, uncomment the line below
+                //setSetpoint(SETPOINT);
+                enable(); 
+                
+                
+                
 	}
 	protected void initDefaultCommand() {
 		this.setDefaultCommand(new arcadeDrive());
@@ -46,4 +71,14 @@ public class Drivetrain extends Subsystem implements RobotMap {
 		rightJ.set(stick.getX() + stick.getY());
 	}
 	public void autoDrive() {}
+
+    protected double returnPIDInput() {
+        return rightmotorchan.getAverageVoltage();  //this only returns rightmotorchan, no left motor 
+    }
+    
+
+    protected void usePIDOutput(double output) {
+        rightmotorcontrol.pidWrite(output);
+        leftmotorcontrol.pidWrite(output);//To change body of generated methods, choose Tools | Templates.
+    }
 }
--- src/com/team1160/assistant/subsystems/Shooter.java
+++ src/com/team1160/assistant/subsystems/Shooter.java
@@ -2,14 +2,22 @@ package com.team1160.assistant.subsystems;
 
 import com.team1160.assistant.RobotMap;
 import com.team1160.assistant.commands.Shooter.neutral;
+import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Jaguar;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
-public class Shooter extends Subsystem implements RobotMap {
+public class Shooter extends PIDSubsystem implements RobotMap {
 	protected static Shooter instance;
 	protected Jaguar jagM;
 	protected Encoder encoder;
+        //speed controller
+        protected final SpeedController shootmotorcontrol;
+        protected final AnalogChannel  encoderchan; 
+        protected final double SETPOINT =0; 
+        protected final double ABSOLUTE_TOLERANCE =0; 
 	public static Shooter getInstance() {
 		if (instance == null) {
 			instance = new Shooter();
@@ -17,9 +25,20 @@ public class Shooter extends Subsystem implements RobotMap {
 		return instance;
 	}
 	private Shooter() {
+                super("Shooter", 0, 0, 0); //numerical values PID will be changed to experimental values
 		jagM = new Jaguar(SHOOT_MOTOR_SLOT, SHOOT_MOTOR_CHAN);
 		encoder = new Encoder(ENC_SHOOT_A, ENC_SHOOT_B, true);
 		encoder.start();
+                //speed control
+                shootmotorcontrol = jagM; 
+                encoderchan = new AnalogChannel(RobotMap.ENC_SHOOT_A); //use only one of the encoder channels
+                getPIDController().setContinuous(false); 
+                LiveWindow.addActuator("Drivetrain", "PIDSubsystem", getPIDController()); 
+                setAbsoluteTolerance(ABSOLUTE_TOLERANCE); 
+                shootmotorcontrol.set(SETPOINT); //I think these set up the setpoints; if not, uncomment the line below
+                //setSetpoint(SETPOINT);
+                enable(); 
+                
 	}
 	protected void initDefaultCommand() {
 		this.setDefaultCommand(new neutral());
@@ -36,4 +55,12 @@ public class Shooter extends Subsystem implements RobotMap {
 	public void letGo() {
 		jagM.set(LET_GO);
 	}
+
+    protected double returnPIDInput() {
+        return encoderchan.getAverageVoltage(); 
+    }
+
+    protected void usePIDOutput(double output) {
+       shootmotorcontrol.pidWrite(output); //To change body of generated methods, choose Tools | Templates.
+    }
 }
